556de3d3489307ceb1dcb04f374dd3a91ab7e90e
BogGoro <troyegubov.den@gmail.com>
2025-06-27 17:56:45 +0300

    feat: add queries for updating and getting score and status

---
+++ src/scripts/pg_connect.py
@@ -0,0 +1,61 @@
+from contextlib import contextmanager
+from typing import Generator
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class PgConnect:
+    def __init__(self, host: str, port: str, db_name: str, user: str, pw: str, sslmode: str = "disable") -> None:
+        self.host = host
+        self.port = int(port)
+        self.db_name = db_name
+        self.user = user
+        self.pw = pw
+        self.sslmode = sslmode
+
+    def url(self) -> str:
+        return """
+            host={host}
+            port={port}
+            dbname={db_name}
+            user={user}
+            password={pw}
+            target_session_attrs=read-write
+            sslmode={sslmode}
+        """.format(
+            host=self.host,
+            port=self.port,
+            db_name=self.db_name,
+            user=self.user,
+            pw=self.pw,
+            sslmode=self.sslmode)
+
+    def client(self):
+        return psycopg2.connect(self.url())
+
+    @contextmanager
+    def connection(self) -> Generator[psycopg2.extensions.connection, None, None]:
+        conn = psycopg2.connect(self.url())
+        try:
+            yield conn
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            raise e
+        finally:
+            conn.close()
+
+
+class PgConnectionBuilder:
+
+    @staticmethod
+    def pg_conn() -> PgConnect:
+        return PgConnect(
+                host="postgres",
+                port=os.getenv("POSTGRES_PORT"),
+                db_name=os.getenv("POSTGRES_DB"),
+                user=os.getenv("POSTGRES_USER"),
+                pw=os.getenv("POSTGRES_PASSWORD")
+            )
--- src/scripts/score.py
+++ src/scripts/score.py
@@ -1,19 +1,33 @@
+from src.scripts.pg_connect import PgConnectionBuilder
 
 class Score:
-    def __init__(self,username, bookId, score):
-        self._username = username
+    def __init__(self, username: str, bookId: int, score: int):
+        self._username: str = username
         self._bookId = bookId
         self._score = score
         self._userid = self.get_userid(username)
+        self.db = PgConnectionBuilder.pg_conn("POSTGRES_DEFAULT")
     
-    def get_userid(self, username):
-        return
+    def get_userid(self) -> str:
+        with self._db.client().cursor() as cur:
+            cur.execute("SELECT id FROM user WHERE username = %(username)s", {"username": self._username})
+            return cur.fetchone()[0]
     
     def get_score(self):
-        return
+        with self._db.client().cursor() as cur:
+            cur.execute("SELECT score FROM score WHERE userid = %(userid)s AND bookid = %(bookid)s", {"userid": self._userid, "bookid": self._bookId})
+            return cur.fetchone()[0]
 
     def set_score(self):
-        return
+        if self.get_score():
+            with self._db.client().cursor() as cur:
+                cur.execute("UPDATE score SET score = %(score)s, updatets = NOW() WHERE userid = %(userid)s AND bookid = %(bookid)s", {"userid": self._userid, "bookid": self._bookId, "score": self._score})
+        else:
+            with self._db.client().cursor() as cur:
+                cur.execute("INSERT INTO score (userid, bookid, score) VALUES (%(userid)s, %(bookid)s, %(score)s)", {"userid": self._userid, "bookid": self._bookId, "score": self._score})
     
+    def drop_score(self):
+        with self._db.client().cursor() as cur:
+            cur.execute("UPDATE score SET isactual = false WHERE userid = %(userid)s AND bookid = %(bookid)s", {"userid": self._userid, "bookid": self._bookId})
 
         
