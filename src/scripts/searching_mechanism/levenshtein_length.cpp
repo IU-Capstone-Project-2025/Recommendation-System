0dd24b5fb31f1824bad7905c45d8308bd406e68d
Abraham14711 <pet_zed@rambler.ru>
2025-07-08 13:04:10 +0300

    feat: start writing the algo

---
+++ src/scripts/searching_mechanism/levenshtein_length.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+#include "fstream"
+#include "vector"
+#include <algorithm> 
+using namespace std;
+
+
+void solve(string book_name){
+    
+}
+
+
+void main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string book_name;
+    cin>>book_name;
+    solve(book_name);
+}
