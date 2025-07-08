7f657361406d0dde57a6a72d3198e84722acd4a9
Abraham14711 <pet_zed@rambler.ru>
2025-07-08 05:23:04 -0700

    feat: add damerau-levenshtein length calculation

--- src/scripts/searching_mechanism/levenshtein_length.cpp
+++ src/scripts/searching_mechanism/levenshtein_length.cpp
@@ -5,9 +5,12 @@
 #include "vector"
 #include <queue>
 #include <algorithm> 
+#include <unordered_map>
 using namespace std;
 
 vector<string> Book_Titles_From_CSV;
+vector<string> ans;
+unordered_map<string,string> Map_With_Most_Similar_Books;
 
 struct Comparator {
     bool operator ()(const pair<string,int>& a, const pair<string,int>&b){
@@ -31,8 +34,12 @@ void read_From_CSV(){
         string firstElement;
         
         if (getline(ss, firstElement, '\n')) {
+            string temp_value(firstElement);
             transform(firstElement.begin(),firstElement.end(),firstElement.begin(),::tolower);
-            if (fl) Book_Titles_From_CSV.push_back(firstElement);
+            string v;
+            for(char c: firstElement) if (c != ' ') v += c;
+            Map_With_Most_Similar_Books[v]=temp_value;
+            if (fl) Book_Titles_From_CSV.push_back(v);
         }
         fl = true;
     }
@@ -45,6 +52,48 @@ void read_From_CSV(){
 void solve(string book_name){
     transform(book_name.begin(),book_name.end(), book_name.begin(),::tolower);
 
+    string v;
+    for(char c: book_name) if (c != ' ') v += c;
+    book_name = v;
+
+    for(auto book_from_csv: Book_Titles_From_CSV){
+        
+        int matrix[book_from_csv.length()+1][book_name.length()+1];
+        
+        for(int i=0;i<book_from_csv.length()+1;++i){
+            matrix[i][0]=i;
+        }
+        for(int i=0;i<book_name.length()+1;++i){
+            matrix[0][i]=i;
+        }
+        
+        for(int i=1; i<=book_from_csv.length(); ++i){
+            for(int j=1; j<=book_name.length();++j){
+                int cost; 
+                if (book_from_csv[i-1]==book_name[j-1] ) cost =0;
+                else cost = 1;
+                matrix [i][j] = min(
+                    matrix[i-1][j]+1,
+                    min(
+                        matrix[i][j-1]+1,
+                        matrix[i-1][j-1]+cost
+                    )
+                );
+                if (i>1 && j>1 && book_from_csv[i-1]==book_name[j-2]&& book_from_csv[i-2]==book_name[j-1]){
+                    matrix[i][j] = min(matrix[i][j], matrix[i-2][j-2]+cost);
+                }
+            }
+        }
+        
+        Priority_Queue_With_Most_Similar_Books.push(make_pair(book_from_csv,matrix[book_from_csv.length()][book_name.length()]));
+    }
+    pair<string,int> first_elem_in_pq = Priority_Queue_With_Most_Similar_Books.top();
+  
+    int cnt =9;
+    while(cnt-->0){
+        ans.push_back(Priority_Queue_With_Most_Similar_Books.top().first);
+        Priority_Queue_With_Most_Similar_Books.pop();
+    }
 }
 
 
@@ -52,11 +101,17 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     read_From_CSV();
-    for(int i=0;i<Book_Titles_From_CSV.size();++i){
-        cout<<Book_Titles_From_CSV[i]<<endl;
-    }
+
     string book_name;
-    cin>>book_name;
+    getline(cin,book_name);
+
+    if (book_name.length()>100) {
+        cerr<<"Book name is too long"<<endl; 
+        return 0;
+    }
     solve(book_name);
+    for(int i=0;i<ans.size();++i){
+        cout<<Map_With_Most_Similar_Books[ans[i]]<<endl;
+    }
     return 0;
 }
