0b58ade7487ab5239eca071b8bb3dca7dbee03f7
Abraham14711 <pet_zed@rambler.ru>
2025-07-08 14:00:11 +0300

    feat: add csv parser for book names

--- src/scripts/searching_mechanism/levenshtein_length.cpp
+++ src/scripts/searching_mechanism/levenshtein_length.cpp
@@ -1,8 +1,36 @@
+
 #include "iostream"
 #include "fstream"
+#include <sstream>
 #include "vector"
 #include <algorithm> 
 using namespace std;
+
+vector<string> Book_Titles_From_CSV;
+
+void read_From_CSV(){
+    string filepath = "titles_only.csv";
+    ifstream file_for_reading(filepath);
+    if (!file_for_reading.is_open()) {
+        cerr << "Не удалось открыть файл: "<<endl;
+        return;
+    }
+    string line;
+    bool fl =false;
+    while (getline(file_for_reading, line)) {
+        stringstream ss(line);
+        string firstElement;
+        
+        if (getline(ss, firstElement, '\n')) {
+            transform(firstElement.begin(),firstElement.end(),firstElement.begin(),::tolower);
+            if (fl) Book_Titles_From_CSV.push_back(firstElement);
+        }
+        fl = true;
+    }
+
+    file_for_reading.close();
+    return;
+}
 
 
 void solve(string book_name){
@@ -10,10 +38,15 @@ void solve(string book_name){
 }
 
 
-void main(){
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    read_From_CSV();
+    for(int i=0;i<Book_Titles_From_CSV.size();++i){
+        cout<<Book_Titles_From_CSV[i]<<endl;
+    }
     string book_name;
     cin>>book_name;
     solve(book_name);
+    return 0;
 }
